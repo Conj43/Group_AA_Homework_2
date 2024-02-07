d9ec3bb32f0c7e6b173e5c69366643f7d63441f9
Drew Distler <dmd9g3@umsystem.edu>
2024-02-07 13:37:18 -0600

    made student array

--- Group_AA_Homework_2/src/edu/mu/StudentManager.java
+++ Group_AA_Homework_2/src/edu/mu/StudentManager.java
@@ -3,12 +3,12 @@ package edu.mu;
 public class StudentManager {
 
 	public StudentManager() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Student[] studentArray;
+		
+		
+		//just trying to get a commit through
+		
 	}
 
 }
