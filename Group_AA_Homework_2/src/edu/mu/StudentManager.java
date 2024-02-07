2910c2e38c7610cc424eefa804df5e632e83c1e5
kymaniscott <kymaniscott@mwc-050203.dhcp.missouri.edu>
2024-02-07 14:41:52 -0600

    It needs the right getters and setters, might have a few issues

--- Group_AA_Homework_2/src/edu/mu/StudentManager.java
+++ Group_AA_Homework_2/src/edu/mu/StudentManager.java
@@ -4,11 +4,29 @@ public class StudentManager {
 
 	public StudentManager() {
 		// TODO Auto-generated constructor stub
+		
+		 public Student search(int id) {
+			 // using a loop to iterate through the students to find the right one
+			 for (Student student : students) {
+				 if (student.getId() == id) {
+					 return student; // find the right student using their id
+					 }
+				 }
+			 }
+		        return null; // Student not found
+		    
 	}
+	// using the id we just got to identify the student we are going to update their grade
+	public boolean updateStudentGradeById(int id, double grade) {
+        Student student = search(id);
+        if (student != null) {
+            student.setGrade(grade);
+            return true; // Student found and updated
+        }
+        return false; // Student ID not found
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	
+	
 }
