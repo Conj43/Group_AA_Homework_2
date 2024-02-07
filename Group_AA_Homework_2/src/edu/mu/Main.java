83b0005ff55a43d4267e03830c696850c8123b81
Drew Distler <dmd9g3@umsystem.edu>
2024-02-07 14:31:12 -0600

    readFromFile and main call to it

--- Group_AA_Homework_2/src/edu/mu/Main.java
+++ Group_AA_Homework_2/src/edu/mu/Main.java
@@ -8,6 +8,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			boolean fileStatus = StudentManager.readFromFile("studentData.txt");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
--- Group_AA_Homework_2/src/edu/mu/StudentManager.java
+++ Group_AA_Homework_2/src/edu/mu/StudentManager.java
@@ -1,14 +1,58 @@
 package edu.mu;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class StudentManager {
+	
+	private Student[] studentArray;
 
 	public StudentManager() {
 		
-		Student[] studentArray;
+		readFromFile("studentData.txt");
 		
-		
-		//just trying to get a commit through
 		
 	}
+	
+	
+
+public boolean readFromFile(String fileName)
+{
+	
+	try
+	{
+		Scanner scan = new Scanner(new FileInputStream(fileName));
+		
+		
+		for(int i = 0; i < 5; i++)
+		{
+			int id = scan.nextInt();
+			String name = scan.next();
+			double grade = scan.nextDouble();
+			scan.next();
+			
+			Student student = new Student();
+			student.setId(id);
+			student.setName(name);
+			student.setGrade(grade);
+			
+			studentArray[i] = student;
+			student.toString();
+			
+		}
+		
+		return true;
+	}catch(FileNotFoundException e)
+	{
+		e.printStackTrace();
+		return false;
+	}
+	
+	
+	
+	
+	
+}
 
 }
