dd4c54069f1adea09ca8f2bd4548420fb4d890a3
kymaniscott <kymaniscott@mwc-050203.dhcp.missouri.edu>
2024-02-08 15:33:16 -0600

    First commit

--- Group_AA_Homework_2/src/edu/mu/StudentManager.java
+++ Group_AA_Homework_2/src/edu/mu/StudentManager.java
@@ -76,6 +76,29 @@ public boolean readFromFile(String fileName)
 		return false; //if the file is not read correctly it will return false
 	}
 	
+		// TODO Auto-generated constructor stub
+		
+		 public Student search(int id) {
+			 // using a loop to iterate through the students to find the right one
+			 for (Student student : students) {
+				 if (student.getId() == id) {
+					 return student; // find the right student using their id
+					 }
+				 }
+			 }
+		        return null; // Student not found
+		    
+	
+	// using the id we just got to identify the student we are going to update their grade 
+	public boolean updateStudentGradeById(int id, double grade) {
+        Student student = search(id);
+        if (student != null) {
+            student.setGrade(grade);
+            return true; // Student found and updated
+        }
+        return false; // Student ID not found
+    }
+	
 	
 	
 	
